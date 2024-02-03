c668fee5e9c76fae23896784807d0c7b0dd6079c
Chun-gu <chungu.yi92@gmail.com>
2024-02-03 20:48:21 +0900

    chore: react-query 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from '@/components/provider';
+
 import reportWebVitals from './reportWebVitals';
 import Router from './Router';
 
@@ -15,7 +17,9 @@ async function enableMocking() {
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <Router />
+      <Provider>
+        <Router />
+      </Provider>
     </React.StrictMode>,
   );
 });
