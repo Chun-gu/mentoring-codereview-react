85d529e90d3e57d59be9f195719ca7e15b36481c
Chun-gu <chungu.yi92@gmail.com>
2024-02-04 23:46:22 +0900

    feat: 디자인에 맞는 유틸리티 클래스로 오버라이드

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,50 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./public/index.html', './src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      'xs': '8px',
+      'sm': '12px',
+      'md': '16px',
+      'lg': '20px',
+      'xl': '24px',
+      '2xl': '28px',
+      '3xl': '32px',
+    },
+    fontWeight: {
+      regular: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+    },
+    lineHeight: {
+      xs: '1.2',
+      sm: '1.3',
+      md: '1.4',
+      lg: '1.5',
+    },
+    colors: {
+      white: '#FFFFFF',
+      gray: {
+        'f4': '#F4F4F4',
+        'd9': '#D9D9D9',
+        'aa': '#AAAAAA',
+        '99': '#999999',
+        '3f': '#3F3F3F',
+        '1a': '#1A1A1A',
+      },
+      black: '#000000',
+      yellow: '#FFBF66',
+      orange: '#FF4800',
+      red: '#EA3800',
+    },
+    borderRadius: {
+      sm: '4px',
+      DEFAULT: '8px',
+      full: defaultTheme.borderRadius.full,
+    },
   },
   plugins: [],
 };
