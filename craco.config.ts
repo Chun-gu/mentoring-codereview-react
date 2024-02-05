99ddea8ecdee50c21a3ea69d87700791151c740a
Chun-gu <chungu.yi92@gmail.com>
2024-02-06 03:06:01 +0900

    build: svg의 경로를 빌드 타입에 주입하기 위한 설정 추가

--- craco.config.ts
+++ craco.config.ts
@@ -1,4 +1,14 @@
 module.exports = {
+  webpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+  },
   plugins: [
     {
       plugin: require('craco-alias'),
