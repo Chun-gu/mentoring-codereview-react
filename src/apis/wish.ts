604dbcafde6202c1e37ad2b9cb692f4d7006644b
Chun-gu <chungu.yi92@gmail.com>
2024-02-06 21:15:35 +0900

    feat: 찜 삭제 요청하는 함수 구현

--- src/apis/wish.ts
+++ src/apis/wish.ts
@@ -8,3 +8,14 @@ export async function addWish(exhibitionId: number): Promise<{ exhibitionId: num
 
   return data;
 }
+
+export async function deleteWish(exhibitionId: number): Promise<{ exhibitionId: number }> {
+  const response = await fetch('/wishes', {
+    method: 'DELETE',
+    body: JSON.stringify({ exhibitionId }),
+  });
+  if (!response.ok) throw new Error('DELETE /wishes');
+  const data = await response.json();
+
+  return data;
+}
