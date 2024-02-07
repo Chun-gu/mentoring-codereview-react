7f8d8d3f750fecb556dcddb5d6a96a4e28f5a639
Chun-gu <chungu.yi92@gmail.com>
2024-02-07 20:12:15 +0900

    feat: 찜목록이 비어있을 때 보여줄 컴포넌트 구현

---
+++ src/components/NoWishes.tsx
@@ -0,0 +1,10 @@
+export default function NoWishes() {
+  return (
+    <article className="flex h-full flex-col items-center justify-center gap-1">
+      <p className="text-md font-regular leading-xs text-gray-1a">찜해놓은 전시회가 없습니다.</p>
+      <p className="text-md font-regular leading-xs text-gray-aa">
+        맘에 드는 전시회가 있다면 찜해보세요
+      </p>
+    </article>
+  );
+}
