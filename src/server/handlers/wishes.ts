db2bb85ae402d0ca9ee63a1ef93be75ea086fd09
Chun-gu <chungu.yi92@gmail.com>
2024-02-07 20:04:55 +0900

    feat: 찜목록 GET 요청에 응답하는 API 엔드포인트 구현

--- src/server/handlers/wishes.ts
+++ src/server/handlers/wishes.ts
@@ -1,6 +1,10 @@
 import { http, HttpResponse } from 'msw';
 
-import type { Wish } from '@/types/exhibition-type';
+import type { Exhibition, Wish } from '@/types/exhibition-type';
+
+import { exhibitions } from '../db';
+
+type GetWishResponseBody = Array<Exhibition>;
 
 type AddWishParams = {};
 type AddWishRequestBody = { exhibitionId: number };
@@ -12,6 +16,15 @@ type DeleteWishResponseBody = { deletedExhibitionId: number };
 
 // TODO: change route to /user/me/wishes
 export const handlers = [
+  http.get<{}, {}, GetWishResponseBody, '/user/me/wishes'>('/user/me/wishes', () => {
+    const wishesData = JSON.parse(localStorage.getItem('wishes') || '[]') as Array<Wish>;
+    const wishes = exhibitions
+      .filter((exhibition) => wishesData.includes(exhibition.id))
+      .map((wish) => ({ ...wish, isWished: true }));
+
+    return HttpResponse.json(wishes);
+  }),
+
   http.post<AddWishParams, AddWishRequestBody, AddWishResponseBody, '/wishes'>(
     '/wishes',
     async ({ request }) => {
@@ -25,6 +38,7 @@ export const handlers = [
       return HttpResponse.json({ wishedExhibitionId: exhibitionId });
     },
   ),
+
   http.delete<DeleteWishParams, DeleteWishRequestBody, DeleteWishResponseBody, '/wishes'>(
     '/wishes',
     async ({ request }) => {
